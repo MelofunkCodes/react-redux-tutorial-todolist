866990c6898efddf95bf180685d3833033982dd2
Melinda Yeoh <myeoh27@gmail.com>
2018-01-11 16:22:02 -0500

    17. React ToDo List Example (Adding a ToDo) - adding input field

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -18,12 +18,21 @@ class ToDoApp extends Component {
 
     return (
       <div>
+        <input ref={(node) => {
+            /*
+             using the ref attribute to link whatever the user types in the input so that we can
+             then access it with this.input.value
+              */
+            this.input = node;
+          }}
+        />
         <button onClick={() => {
           store.dispatch({
             type: 'ADD_TODO',
-            text: 'Test',
-            id: nextToDoId++,
+            text: this.input.value,
+            id: nextToDoId += 1,
           });
+          this.input.value = ''; // clears the input field after button is clicked
         }}>
           Add Todo
         </button>
