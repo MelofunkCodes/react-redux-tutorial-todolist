0f00be594f4ecd8dbd30ca923407ead62b3c536b
Melinda Yeoh <myeoh27@gmail.com>
2018-01-11 12:03:34 -0500

    13. Redux: Reducer Composition with Arrays

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,26 +1,37 @@
+// Child reducer that looks at each todo vs the array of todos
+const todo = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text,
+        completed: false,
+      };
+    case 'TOGGLE_TODO':
+      return (state.id === action.id) ? { ...state, completed: !state.completed }
+        : state;
+    /*
+    If getting an error message above with using the spread operator in object literal,
+    uncomment out below
+    */
+    // case 'TOGGLE_TODO':
+    //   return (state.id === action.id) ? Object.assign({}, state, { completed: !state.completed })
+    //     : state;
+    default:
+      return state;
+  }
+};
+
+// Top-level Parent Reducer
 const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false,
-        },
+        todo(undefined, action),
       ];
     case 'TOGGLE_TODO':
-      return state.map(todo =>
-        ((todo.id === action.id) ? { ...todo, completed: !todo.completed }
-          : todo));
-    /*
-     If getting an error message above with using the spread operator in object literal,
-      uncomment out below
-      */
-    // case 'TOGGLE_TODO':
-    //   return state.map( todo =>
-    //     ((todo.id === action.id) ? Object.assign({}, todo, { completed: !todo.completed })
-    //       : todo));
+      return state.map(t => todo(t, action));
     default:
       return state;
   }
