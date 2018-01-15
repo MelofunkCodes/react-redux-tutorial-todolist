7a9a6d31f5ade8f07bb2724c95dd522e212e581c
Melinda Yeoh <myeoh27@gmail.com>
2018-01-15 17:37:45 -0500

    29. Generating Containers with connect() from React Redux (FilterLink)

--- src/containers/FilterLink.jsx
+++ src/containers/FilterLink.jsx
@@ -1,50 +1,40 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import Link from '../components/Link';
 
-class FilterLink extends Component {
-  /*
-   Need to subscribe to store changes
-   - So we will move subscription to the store to the React life cycle methods
-   - forceUpdate: forces a re-rendering of the component. This is called anytime the store changes
-    */
-  componentDidMount() {
-    const { store } = this.context;
+/*
+In this case, we need to compare the filter prop passed to FilterLink from Footer.jsx...
+to the visibilityFilter in the Redux store's state.
 
-    // Declaring the unsubscribe function in componentDidMount
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
-  }
-
-  // important to unsubscribe as well to clean up the subscription
-  componentWillUnmount() {
-    // this is the *return* value of the store.subscribe() method above in componentDidMount
-    this.unsubscribe();
-  }
-
-  render() {
-    const { props } = this;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <Link
-        active={props.filter === state.visibilityFilter}
-        onClick={() =>
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter,
-          })
-        }
-      >
-        {props.children}
-      </Link>
-    );
-  }
-}
-
-FilterLink.contextTypes = {
-  store: PropTypes.object,
+To make it clear, we will rename it ownProps, which will indicate the container component's own
+props. not the props passed to the child, which is the return value of mapStateToProps.
+ */
+const mapStateToProps = (
+  state,
+  ownProps,
+) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter,
+  };
 };
+
+const mapDispatchToProps = (
+  dispatch,
+  ownProps,
+) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter,
+      });
+    },
+  };
+};
+
+const FilterLink = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Link);
 
 export default FilterLink;
