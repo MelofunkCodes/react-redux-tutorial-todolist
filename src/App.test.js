cb2275cc7b5747c796a7c3902d5a1a3cb4557f31
Melinda Yeoh <myeoh27@gmail.com>
2018-01-15 14:40:46 -0500

    11. Writing a Todo List Reducer (Adding a Todo) - got tests to work for the todo reducer

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import deepFreeze from 'deep-freeze';
+
+import App from './components/App';
+import todos from './reducers/todos';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+
+it('Adds a todo', () => {
+
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux',
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter);
+});
+
+it('Toggles a todo', () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false,
+    },
+  ];
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 0,
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: true,
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false,
+    },
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(todos(stateBefore, action)).toEqual(stateAfter)
 });
