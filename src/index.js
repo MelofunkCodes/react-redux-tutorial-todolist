e0afaffebeac28634fc157ca33033bbafe32f2aa
Melinda Yeoh <myeoh27@gmail.com>
2018-01-15 15:40:43 -0500

    25. Passing the Store Down Implicitly via Context - added Provider component to wrap around ToDoApp and pass the context of store to the child and grandchild components that may need it

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import { createStore } from 'redux';
 
 import './index.css';
@@ -12,10 +13,33 @@ import todoApp from './reducers/index';
 // Components
 import ToDoApp from './components/ToDoApp';
 
-const store = createStore(todoApp);
+
+class Provider extends Component {
+  /*
+   This method is what makes the store available to any component that it renders.
+   Now the store will be available to any children and grandchildren of the components Provider
+   renders. In this case, the ToDoApp and any presentational components and containers built inside
+   of it!
+    */
+  getChildContext() {
+    return {
+      store: this.props.store, // This corresponds to the `store` passed in as a prop
+    };
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+Provider.childContextTypes = {
+  store: PropTypes.object,
+};
 
 ReactDOM.render(
-  <ToDoApp store={store} />,
+  <Provider store={createStore(todoApp)}>
+    <ToDoApp />
+  </Provider>,
   document.getElementById('root'),
 );
 
