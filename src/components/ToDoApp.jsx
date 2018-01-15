1aadf7e0b745f8371aba3dcb4ee6fcf0724a765b
Melinda Yeoh <myeoh27@gmail.com>
2018-01-15 12:05:33 -0500

    moved VisibleTodoList to its own container component

--- src/components/ToDoApp.jsx
+++ src/components/ToDoApp.jsx
@@ -1,76 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './ToDoApp.css';
 import AddTodo from './AddTodo';
-import TodoList from './TodoList';
+import VisibleTodoList from '../containers/VisibleTodoList';
 import Footer from './Footer';
 
 
-const getVisibleTodos = (
-  todos,
-  filter,
-) => {
-  switch (filter) {
-    case 'SHOW_ALL':
-      return todos;
-    case 'SHOW_ACTIVE':
-      return todos.filter(t => !t.completed);
-    case 'SHOW_COMPLETED':
-      return todos.filter(t => t.completed);
-    default:
-      return todos;
-  }
-};
-
-const ToDoApp = ({ store }) => {
-  return (
-    <div className="todoApp">
-      <AddTodo
-        store={store}
-      />
-      <VisibleTodoList
-        store={store}
-      />
-      <Footer
-        store={store}
-      />
-    </div>
-  );
-}
+const ToDoApp = ({ store }) => (
+  <div className="todoApp">
+    <AddTodo
+      store={store}
+    />
+    <VisibleTodoList
+      store={store}
+    />
+    <Footer
+      store={store}
+    />
+  </div>
+);
 
 export default ToDoApp;
-
-
-// Container Components
-
-class VisibleTodoList extends Component {
-  componentDidMount() {
-    const { store } = this.props;
-
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const { props } = this;
-    const { store } = props;
-    const state = store.getState();
-
-    return (
-      <TodoList
-        todos={getVisibleTodos(
-          state.todos,
-          state.visibilityFilter,
-        )}
-        onTodoClick={id => store.dispatch({
-          type: 'TOGGLE_TODO',
-          id,
-        })
-        }
-      />
-    );
-  }
-}
